89b80a0601e52124c2c42ff4e1bd9c187a3289f8
Prashant Bhosale <bhosale.prashant09@gmail.com>
2024-05-16 23:07:21 +0530

    removed unwanted comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,10 @@
 module.exports = {
     extends: [
       'plugin:cypress/recommended',
-      // Add other configurations as needed
     ],
     plugins: ['prettier'],
     rules: {
       'prettier/prettier': 'error',
-      // Add other rules as needed
     },
   };
   
