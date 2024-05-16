0949575a610268703fcc764971b91ec007f73b1e
Prashant Bhosale <bhosale.prashant09@gmail.com>
2024-05-16 22:32:30 +0530

    added README file and added comments to cypress.config file

---
+++ README.md
@@ -0,0 +1,24 @@
+# cypress-regression-tests
+
+## How To Get Started
+
+- Download and install Node.js, if not already done. You can do it from [nodejs.org](https://nodejs.org/en/download/)
+- Clone this [repo](https://github.com/prashant7890/cypressTest.git)
+- Then inside your editors terminal run the command
+
+  `npm install cypress --save-dev`
+
+- Next, to run tests locally on Cypress runner you'll have to run this command in your terminal
+
+  `npx cypress open`
+
+  This will open a Cypress window where you can run tests
+
+- To run all the tests in headless mode you'll have to run this command in your terminal
+
+  `npx cypress run`
+
+## Notes
+- I have kept task.html file on root level
+- All the tests are written in file easyGeneratorTest.cy.js
+
--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,26 @@
+// Importing the 'defineConfig' function from the 'cypress' module
 const { defineConfig } = require("cypress");
-
+// Importing the 'fs' module for file system operations
 const fs = require('fs');
 
+// Exporting a Cypress configuration object using 'defineConfig' function
 module.exports = defineConfig({
+  // Configuration for end-to-end tests
   e2e: {
+    // Setup node events
     setupNodeEvents(on, config) {
+      // Define a custom task named 'readTextFromFile'
       on('task', {
         readTextFromFile(filePath) {
+          // Return a promise to asynchronously read text from the file
           return new Promise((resolve, reject) => {
+            // Read the file asynchronously using 'fs.readFile'
             fs.readFile(filePath, 'utf8', (err, data) => {
               if (err) {
+                // If an error occurs, reject the promise
                 reject(err);
               } else {
+                // If successful, resolve the promise with the file data
                 resolve(data);
               }
             });
